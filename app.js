62c1cb34171adafa4f1d866722355ff985053eb4
Vrum Vrum <vrum.com.au@gmail.com>
2016-02-05 14:49:02 +1100

    Create player end point for users
    known issue
    - Mongoose DB not setup properly.
    
    [#109939730]

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var users = require('./server/routes/users');
 var app = express();
+var db = require('./server/model/db');
+var players = require('./server/routes/players');
 
+app.use('/users', users);
 app.use('', express.static(__dirname + '/'));
+
+app.use('/players', players);
 
 app.get('/api/users', function (req, res) {
     res.json({
--- app/server/model/players.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var playerSchema = new mongoose.Schema({
-    _id: Number,
-    email: Number,
-    name: {
-        first: String,
-        last: String
-    }
-});
