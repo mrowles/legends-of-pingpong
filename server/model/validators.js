81f0974481f56e83740dd96847768354f88bdaeb
Vrum Vrum <vrum.com.au@gmail.com>
2016-02-09 18:06:28 +1100

    Removes console log

--- server/model/validators.js
+++ server/model/validators.js
@@ -1,7 +1,6 @@
 var validators = function () {
   return {
     email: function (email) {
-      console.log(1, email);
       var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       return re.test(email)
     }
