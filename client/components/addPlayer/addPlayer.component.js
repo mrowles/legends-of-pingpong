b7384eccc9c6260322996645f53d21b936176301
Vrum Vrum <vrum.com.au@gmail.com>
2016-02-26 15:15:34 +1100

    updates Angular to the latest file

--- client/components/addPlayer/addPlayer.component.js
+++ client/components/addPlayer/addPlayer.component.js
@@ -1,4 +1,6 @@
-System.register(['angular2/core', 'angular2/http', '../../model/player/player.model', 'rxjs/Rx'], function(exports_1) {
+System.register(['angular2/core', 'angular2/http', '../../model/player/player.model', 'rxjs/Rx'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -55,7 +57,7 @@ System.register(['angular2/core', 'angular2/http', '../../model/player/player.mo
                     __metadata('design:paramtypes', [http_1.Http])
                 ], AddPlayerComponent);
                 return AddPlayerComponent;
-            })();
+            }());
             exports_1("AddPlayerComponent", AddPlayerComponent);
         }
     }
--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,7 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {ROUTER_PROVIDERS} from 'angular2/router';
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
 
+import {bootstrap} from 'angular2/platform/browser'
 import {AppComponent} from './app.component';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
 bootstrap(AppComponent, [ROUTER_PROVIDERS]);
