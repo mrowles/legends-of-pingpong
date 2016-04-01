060d760aff8b8b7314ebed9923dd9cbd8951019f
Travis CI <project-disney@pivotal.io>
2016-04-01 13:56:03 +1100

    Adds improved error logging in Mongo

--- server/model/db.js
+++ server/model/db.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connection;
 
-db.on('error', console.error('Mongodb failed to start'));
+db.on('error', function (err) {
+  console.error('Mongodb failed to start', err);
+});
+
 db.once('open', function () {
   console.log('Mongodb started');
 });
