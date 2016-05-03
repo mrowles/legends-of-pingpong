426ee06cb1e30ae1d019342c4606ccd58672da89
Vrum Vrum <vrum.com.au@gmail.com>
2016-05-03 19:58:20 +1000

    Add server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var db = require('./server/model/db');
+var bodyParser = require('body-parser');
+
+var index = require('./server/routes/index');
+var players = require('./server/routes/players');
+var matches = require('./server/routes/matches');
+
+var app = express();
+
+//app.set('views', path.join(__dirname, '/'));
+//app.set('view engine', 'ejs');
+//app.engine('html', require('ejs').renderFile);
+
+
+app.use(express.static("./node_modules/"));
+app.use(express.static("./client/"));
+
+//app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, '/')));
+
+app.use(bodyParser.json());
+
+app.use('/api/player', players);
+app.use('/api/match', matches);
+
+//app.use('/*', index);
+
+var port = process.env.PORT || 8765;
+
+app.listen(port);
+app.on('error', onError);
+app.on('listening', onListening);
+
+function onListening() {
+  console.log('Legends of Pong app listening on port ' + port + '!');
+}
+
+function onError(err) {
+  console.error('Failed to listen on port ' + port, err);
+}
+
+module.exports = app;
