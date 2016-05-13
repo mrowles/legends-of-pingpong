2f0aa926f9b908dc7f4a66fb5b3e5c625c441d3e
Travis CI <project-disney@pivotal.io>
2016-05-13 13:43:51 +1000

    Unfinished state

--- client/main.ts
+++ client/main.ts
@@ -1,7 +1,6 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {ROUTER_PROVIDERS}  from '@angular/router-deprecated';
-
 import {AppComponent} from 'js/app.component';
+import {ROUTER_PROVIDERS}  from '@angular/router-deprecated';
 import {PlayerService} from 'js/service/player.service';
 import {MatchService} from 'js/service/match.service';
 
--- public/js/app.component.js
+++ public/js/app.component.js
@@ -1,53 +1,78 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var core_1 = require('@angular/core');
-var router_deprecated_1 = require('@angular/router-deprecated');
-var addPlayer_component_1 = require('js/components/addPlayer/addPlayer.component');
-var homepage_component_1 = require('js/components/homepage/homepage.component');
-var createMatch_component_1 = require('js/components/createMatch/createMatch.component');
-var matchPage_component_1 = require('js/components/matchPage/matchPage.component');
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = 'LOAD GODAMMIT';
-    }
-    AppComponent.prototype.ngOnInit = function () {
-        console.log('TEST');
+System.register(['@angular/core', '@angular/router-deprecated', 'js/components/addPlayer/addPlayer.component', 'js/components/homepage/homepage.component', 'js/components/createMatch/createMatch.component', 'js/components/matchPage/matchPage.component'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    AppComponent = __decorate([
-        core_1.Component({
-            directives: [router_deprecated_1.ROUTER_DIRECTIVES],
-            providers: [
-                router_deprecated_1.ROUTER_PROVIDERS,
-            ],
-            selector: 'lopp-app',
-            templateUrl: 'js/app.html'
-        }),
-        router_deprecated_1.RouteConfig([
-            {
-                component: homepage_component_1.HomepageComponent,
-                name: 'Home',
-                path: '/',
-                useAsDefault: true
-            }, {
-                component: addPlayer_component_1.AddPlayerComponent,
-                name: 'PlayerAdd',
-                path: '/players/add'
-            }, {
-                component: createMatch_component_1.CreateMatchPage,
-                name: 'CreateMatch',
-                path: '/createMatch'
-            }, {
-                component: matchPage_component_1.MatchPage,
-                name: 'MatchPage',
-                path: '/match'
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, router_deprecated_1, addPlayer_component_1, homepage_component_1, createMatch_component_1, matchPage_component_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
             },
-        ])
-    ], AppComponent);
-    return AppComponent;
-}());
-exports.AppComponent = AppComponent;
+            function (router_deprecated_1_1) {
+                router_deprecated_1 = router_deprecated_1_1;
+            },
+            function (addPlayer_component_1_1) {
+                addPlayer_component_1 = addPlayer_component_1_1;
+            },
+            function (homepage_component_1_1) {
+                homepage_component_1 = homepage_component_1_1;
+            },
+            function (createMatch_component_1_1) {
+                createMatch_component_1 = createMatch_component_1_1;
+            },
+            function (matchPage_component_1_1) {
+                matchPage_component_1 = matchPage_component_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.title = 'LOAD GODAMMIT';
+                }
+                AppComponent.prototype.ngOnInit = function () {
+                    console.log('TEST');
+                };
+                AppComponent = __decorate([
+                    core_1.Component({
+                        directives: [router_deprecated_1.ROUTER_DIRECTIVES],
+                        providers: [
+                            router_deprecated_1.ROUTER_PROVIDERS,
+                        ],
+                        selector: 'lopp-app',
+                        templateUrl: 'js/app.html',
+                    }),
+                    router_deprecated_1.RouteConfig([
+                        {
+                            component: homepage_component_1.HomepageComponent,
+                            name: 'Home',
+                            path: '/',
+                            useAsDefault: true,
+                        }, {
+                            component: addPlayer_component_1.AddPlayerComponent,
+                            name: 'PlayerAdd',
+                            path: '/players/add',
+                        }, {
+                            component: createMatch_component_1.CreateMatchPage,
+                            name: 'CreateMatch',
+                            path: '/createMatch',
+                        }, {
+                            component: matchPage_component_1.MatchPage,
+                            name: 'MatchPage',
+                            path: '/match',
+                        },
+                    ]), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            })();
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
