efcdbf2841bf8f9c5c69e01e012479c4381639eb
Travis CI <project-disney@pivotal.io>
2016-06-24 15:41:31 +1000

    Moving webpack into dependencies to see if it works - updating path for index.html, also logging to see what root is

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function (req, res) {
-  res.sendFile('index.html', {root: '/../../public/'});
+  console.log(__dirname);
+  console.log(path.join(__dirname, '/public'));
+  res.sendFile('index.html', {root: '/app/public/'});
 });
 
 module.exports = router;
